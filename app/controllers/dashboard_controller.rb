02212687a62e1d2c7725aab78fd9ecf9043d382f
Kyle <kyle@rails-srv-01.(none)>
2012-01-19 18:18:37 -0600

    20120119:18-18

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,24 +1,9 @@
 class DashboardController < ApplicationController
+  require 'twilio-ruby'
   layout 'dashboard'
   
   def index
-    #begin
-      #create client
-      #client = Twilio::REST::Client.new( TwilioAccountSID , TwilioAuthToken)
-      #gets the call list
-      #call_list = client.account.calls.list(:status => 'in-progress')
-      
-      #call_list that is displayed in the view
-      #@call_list = []
-      #call_list.each do |call|
-        #@call_list << call
-      #end
-      #set the count for the view
-      #@call_count = call_list.count.to_s     
-    #rescue StandardError => bang
-      #redirect_to :action => '.', 'msg' => "Error #{bang}"
-      #return
-    #end
+
   end
   
   def update_call_list
@@ -42,16 +27,15 @@ class DashboardController < ApplicationController
   def update_waiting_room_list
       #create client
       client = Twilio::REST::Client.new( TwilioAccountSID , TwilioAuthToken)
+     
+      #Find Conference by friendly name
+      conf_waiting_room = client.account.conferences.list(:FriendlyName => 'Waiting Room')
       
-      #conf_waiting_room = client.account.conferences.list(:FriendlyName => 'WaitingRoom')
-      
-      waiting_participants = client.account.conferences.get('CF827b8578e057de54bbb6263ed59410ad').participants
-      
-      #gets the participant list
-      #waiting_participants = conf_waiting_room.participants
-      
+      #get participant list from twilio
+      waiting_participants = client.account.conferences.get('CF58122a3cb34c67f9a799c395903e9d59').participants 
+            
       @waiting_participants = []
-      waiting_participants.list.each do |call|
+      waiting_participants.each do |call|
         @waiting_participants << call
       end
       #set the count for the view
