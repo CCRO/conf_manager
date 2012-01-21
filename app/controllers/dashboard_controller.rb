2e8fec7008cf4adb6b3c8812798a0127579796e2
Kyle Luzny <kluzny@gmail.com>
2012-01-20 19:28:11 -0600

    20120120:19-28

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -29,34 +29,20 @@ class DashboardController < ApplicationController
   def update_waiting_room_list
       #create client
       client = Twilio::REST::Client.new( TwilioAccountSID , TwilioAuthToken)
-     
-      #Find Conference by friendly name
-      #conf_waiting_room = client.account.conferences.list(:FriendlyName => 'Waiting Room')
       
       #get participant list from twilio
       @waiting_participants = []
       @conference_sid_in_use = ""
       
-      #waiting_participants = client.account.conferences.get('Waiting Room').participants 
-      waiting_participants = client.account.conferences.list(:FriendlyName => 'Waiting Room')
-      waiting_participants.list.each do |call|
-          @waiting_participants << client.account.conferences.get('Waiting Room').participants.get(call)
-      end 
-
-      #client.account.conferences.list.each do |conf|
-      #Conference.all.each do |conf|
-      #  waiting_participants = client.account.conferences.get(conf.sid).participants
-      #  waiting_participants.list.each do |call|
-      #    @waiting_participants << client.account.conferences.get(conf.sid).participants.get(call)
-      #    if !@waiting_participants.empty?
-      #      @conference_sid_in_use = conf.sid.to_s
-      #    end
-      #  end
-      #end
-      
-      #waiting_participants.list.each do |call|
-      #  @waiting_participants << call
-      #end
+      waiting_room_confs  = client.account.conferences.list(:FriendlyName => 'Waiting Room')
+      waiting_room_confs.each do |conf|
+        tmp_participants = client.account.conferences.get(conf.sid).participants
+        tmp_participants.list.each do |caller|
+          #@waiting_participants << caller.call_sid
+          @waiting_participants << client.account.calls.get(caller.call_sid)
+        end
+      end
+    
       #set the count for the view
       @wait_count = @waiting_participants.count.to_s
       respond_to do |format|
