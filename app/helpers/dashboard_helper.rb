0115064c8489c7a509a7abe8c13f338a5eb907cf
Kyle Luzny <kluzny@gmail.com>
2012-01-24 21:17:20 -0600

    20120124:21-17

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,19 @@
 module DashboardHelper
+  def get_caller_name(call)
+
+    if call.direction == 'inbound'
+      caller = Contact.where("phone like ?", call.from[2..-1]).first
+    elsif call.direction == 'outbound'
+      caller = Contact.where("phone like ?", call.to[2..-1]).first
+    end
+    
+    if !caller.nil?
+      callerstr = caller.user
+    elsif !call.caller_name.nil? or !call.caller_name.empty?
+      callerstr = call.caller_name
+    else
+     callerstr = 'Unknown Caller'     
+    end 
+    return callerstr    
+  end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,3 @@
 class Contact < ActiveRecord::Base
+  attr_accessible :user
 end
