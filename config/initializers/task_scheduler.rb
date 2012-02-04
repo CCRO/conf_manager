a8dc8a3b8e242ca1130b334e47836a81765aee73
Kyle Luzny <kluzny@gmail.com>
2012-02-04 06:46:00 -0600

    weird ajax duplication error on production

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -7,10 +7,10 @@ scheduler = Rufus::Scheduler.start_new
 #update the active calls
 scheduler.every("2000") do
   ActiveCall.update_call_list
+  ActiveConference.update_conference_list
 end
 
 #update the active conferences
-scheduler.every("2000") do
-  ActiveConference.update_conference_list
+scheduler.every("3000") do
   ActiveConference.update_all_participant_lists
 end
