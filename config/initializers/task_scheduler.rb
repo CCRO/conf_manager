0683acaf47e4847b943e8a0b7d3b8cc34950fa20
Kyle Luzny <kluzny@gmail.com>
2012-02-07 08:54:45 -0600

    20120207:08-54

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -5,17 +5,20 @@ require 'rufus/scheduler'
 scheduler = Rufus::Scheduler.start_new
 
 #update the active calls
-scheduler.every("2000") do
+scheduler.every("2500") do
   ActiveCall.update_call_list
   ActiveConference.update_conference_list
+  ActiveConference.update_all_participant_lists
+  ActiveRecord::Base.connection_pool.clear_stale_cached_connections!
+  ActiveRecord::Base.connection_pool.clear_reloadable_connections!  
 end
 
 #update the active conferences
 scheduler.every("3000") do
-  ActiveConference.update_all_participant_lists
+  #ActiveConference.update_all_participant_lists
 end
 
 scheduler.every("5000") do
-  ActiveRecord::Base.connection_pool.clear_reloadable_connections!
-  ActiveRecord::Base.connection_pool.clear_stale_cached_connections!
+  #ActiveRecord::Base.connection_pool.clear_stale_cached_connections!
+  #ActiveRecord::Base.connection_pool.clear_reloadable_connections!
 end
