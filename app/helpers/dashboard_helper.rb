7053f5e2f28f6d66308a0c9ca8dcda77652772df
Kyle Luzny <kluzny@gmail.com>
2012-02-07 01:25:01 -0600

    20120207:01-25

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -19,7 +19,9 @@ module DashboardHelper
   
   def link_to_hangup_call(sid)
     #attempt to select all of them
-    js = '$("[id$=' + sid + ']").find(":first").attr("class","btn hangup_phone"); var url = "' + BASE_URL + '/dashboard/hangup_call_immediate/?sid=' + sid + '"; $.post(url);'
+    js = '$("[id$=' + sid + ']").find(":first").attr("class","btn hangup_phone");'
+    js += 'var url = "' + BASE_URL + '/dashboard/hangup_call_immediate/?sid=' + sid + '"; $.post(url);'
+    js += '$("[id$=' + sid + ']").find(":first").fadeOut(1000).delay(500);'
     #this only selects the one that was clicked
     #js = '$(this).parent().parent().attr("class","btn hangup_phone"); var url = "' + BASE_URL + '/dashboard/hangup_call_immediate/?sid=' + sid + '"; $.post(url);'
     link_to_function image_tag('/redx.png', :style => "float: right; margin-right: -10px;" ), js, :confirm => "Hang up call?"
--- app/views/ivr/conference_hold.xml.builder
+++ app/views/ivr/conference_hold.xml.builder
@@ -3,7 +3,7 @@ xml.Response do
 	xml.Gather(:action => @post_to, :numDigits => 4) do
 		xml.Pause :length => 1
 		xml.Say "To join a conference. Enter your 4 digit PIN number." 
-		xml.Say "Press 9 to return to the root menu."
+		xml.Say "Press 9 to go to the root menu."
         xml.Pause :length => 5
         xml.Say "Sorry, We did not hear a response."
     end
